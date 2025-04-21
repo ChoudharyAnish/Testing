363d3c49737a599653d6333f5e882dccfe45b7f7
Anish Choudhary <anishchoudhary022000@gmail.com>
2025-04-21 20:53:24 +0530

    Update homepage.spec.js

--- test/homepage.spec.js
+++ test/homepage.spec.js
@@ -2,6 +2,6 @@ const { test, expect } = require('@playwright/test');
 
 test('Homepage should contain welcome message', async ({ page }) => {
   await page.goto('https://testing-xlem.onrender.com/index');
-  await expect(page).toHaveTitle(/My Advanced App/i);
-  await expect(page.locator('h1')).toHaveText(/My Advanced App/); // adjust if your h1 is different
+  await expect(page).toHaveTitle(/Automation Testing UI/i);
+  await expect(page.locator('h1')).toHaveText(/MAutomation Testing Playground/); // adjust if your h1 is different
 });
