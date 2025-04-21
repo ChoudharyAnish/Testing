1c9ce38f6aa5dac5d7a1bd264871dc7cfed4826f
Anish Choudhary <anishchoudhary022000@gmail.com>
2025-04-21 22:26:08 +0530

    Update homepage.spec.js

--- test/homepage.spec.js
+++ test/homepage.spec.js
@@ -1,5 +1,5 @@
 test('Homepage should contain welcome message', async ({ page }) => {
-  await page.goto('https://testing-xlem.onrender.com/index', { timeout: 60000 }); // Increase timeout to 60 seconds
+  await page.goto('https://testing-xlem.onrender.com', { timeout: 60000 }); // Increase timeout to 60 seconds
   await expect(page).toHaveTitle(/Automation Testing UI/i);
   await expect(page.locator('h1')).toHaveText(/Automation Testing Playground/);
 });
